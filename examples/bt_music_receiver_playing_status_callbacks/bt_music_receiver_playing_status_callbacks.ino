43a0bd01bad16d8c8094c8c8e5bf90b239384fb2
Kevin Jayne <kevin.jayne@injectivelabs.org>
2025-12-30 14:08:24 -0500

    formatting

--- examples/bt_music_receiver_playing_status_callbacks/bt_music_receiver_playing_status_callbacks.ino
+++ examples/bt_music_receiver_playing_status_callbacks/bt_music_receiver_playing_status_callbacks.ino
@@ -20,43 +20,43 @@ I2SStream out;
 BluetoothA2DPSink a2dp_sink(out);
 
 void avrc_rn_play_pos_callback(uint32_t play_pos) {
-  Serial.printf("Play position is %d (%d seconds)\n", play_pos, (int)round(play_pos/1000.0));
+  Serial.printf("Play position is %d (%d seconds)\n", play_pos,
+                (int)round(play_pos / 1000.0));
 }
 
 void avrc_rn_playstatus_callback(esp_avrc_playback_stat_t playback) {
   switch (playback) {
-    case esp_avrc_playback_stat_t::ESP_AVRC_PLAYBACK_STOPPED:
-      Serial.println("Stopped.");
-      break;
-    case esp_avrc_playback_stat_t::ESP_AVRC_PLAYBACK_PLAYING:
-      Serial.println("Playing.");
-      break;
-    case esp_avrc_playback_stat_t::ESP_AVRC_PLAYBACK_PAUSED:
-      Serial.println("Paused.");
-      break;
-    case esp_avrc_playback_stat_t::ESP_AVRC_PLAYBACK_FWD_SEEK:
-      Serial.println("Forward seek.");
-      break;
-    case esp_avrc_playback_stat_t::ESP_AVRC_PLAYBACK_REV_SEEK:
-      Serial.println("Reverse seek.");
-      break;
-    case esp_avrc_playback_stat_t::ESP_AVRC_PLAYBACK_ERROR:
-      Serial.println("Error.");
-      break;
-    default:
-      Serial.printf("Got unknown playback status %d\n", playback);
+  case esp_avrc_playback_stat_t::ESP_AVRC_PLAYBACK_STOPPED:
+    Serial.println("Stopped.");
+    break;
+  case esp_avrc_playback_stat_t::ESP_AVRC_PLAYBACK_PLAYING:
+    Serial.println("Playing.");
+    break;
+  case esp_avrc_playback_stat_t::ESP_AVRC_PLAYBACK_PAUSED:
+    Serial.println("Paused.");
+    break;
+  case esp_avrc_playback_stat_t::ESP_AVRC_PLAYBACK_FWD_SEEK:
+    Serial.println("Forward seek.");
+    break;
+  case esp_avrc_playback_stat_t::ESP_AVRC_PLAYBACK_REV_SEEK:
+    Serial.println("Reverse seek.");
+    break;
+  case esp_avrc_playback_stat_t::ESP_AVRC_PLAYBACK_ERROR:
+    Serial.println("Error.");
+    break;
+  default:
+    Serial.printf("Got unknown playback status %d\n", playback);
   }
 }
 
 void avrc_rn_track_change_callback(uint8_t *id) {
   Serial.println("Track Change bits:");
-  for (uint8_t i = 0; i < 8; i++)
-  {
-    Serial.printf("\tByte %d : 0x%x \n",i,id[i]);
+  for (uint8_t i = 0; i < 8; i++) {
+    Serial.printf("Byte %d : 0x%x \n", i, id[i]);
   }
-  //An example of how to project the pointer value directly as a uint8_t
+  // An example of how to project the pointer value directly as a uint8_t
   uint8_t track_change_flag = *id;
-  Serial.printf("\tFlag value: %d\n",track_change_flag);
+  Serial.printf("Flag value: %d\n", track_change_flag);
 }
 
 void setup() {
@@ -67,11 +67,9 @@ void setup() {
 
   // print instructions
   Serial.println("Connect e.g. from your Phone");
-  while(!a2dp_sink.is_connected()) 
+  while (!a2dp_sink.is_connected())
     delay(1000);
   Serial.println("Play or pause music to test callbacks.");
 }
 
-void loop() {
-  delay(1000);
-}
+void loop() { delay(1000); }
